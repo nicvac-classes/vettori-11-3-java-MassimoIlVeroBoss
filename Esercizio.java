8efe200fb8c15e20ee7a993e64ee5a24af460ff5
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2025-01-24 17:35:04 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,91 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Arrays;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
+    public static void eliminaDuplicati(int[] V) {
+
+    }
+
+    public static void eliminaElemento(int[] V, int posizione) {
+
+    }
+
+    public static void inserisciElemento(int[] V, int posizione, int valore) {
+
+    }
+
+    public static int leggiComando() {
+        Scanner in = new Scanner (System.in);
+        int scelta;
+        do {
+            System.out.println("------ MENU' VETTORI ------");
+            System.out.println("1. Inserisci elemento");
+            System.out.println("2. Elimina elemento");
+            System.out.println("3. Ricerca elemento");
+            System.out.println("4. Elimina duplicati");
+            System.out.println("5. Visualizza vettore");
+            System.out.println("6. Esci");
+            scelta = Integer.parseInt(in.nextLine());
+        } while(scelta<1 || scelta>6);
+        in.close();
+        return scelta;
+    }
+
+    public static int ricercaElemento(int[] V, int valore) {
+        int indice = 0;
+        return indice;
+    }
+
+    public static void visualizzaVettore(int[] V) {
+        int i;
+        for (i=0; i<=V.length-1; i++) {
+            System.out.println("Elemento: " + i + " = " + V[i]);
+        }
+    }
+
+    public static void main(String args[]) { 
+
         Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        int N, pos, x, scelta;
+        System.out.println("Inserire la grandezza del vettore: ");
+        N = Integer.parseInt(in.nextLine());
+        int[] V = new int[N];
+        Arrays.fill(V, 0);
+        do {
+            scelta = leggiComando();
+            switch (scelta) {
+            case 1:
+                System.out.println("Inserire la posizione: ");
+                pos = Integer.parseInt(in.nextLine());
+                System.out.println("Inserire il valore: ");
+                x = Integer.parseInt(in.nextLine());
+                inserisciElemento(V, pos, x);
+                break;
+            case 2:
+                System.out.println("Inserire la posizione: ");
+                pos = Integer.parseInt(in.nextLine());
+                eliminaElemento(V, pos);
+                break;
+            case 3:
+                System.out.println("Inserire il valore: ");
+                x = Integer.parseInt(in.nextLine());
+                ricercaElemento(V, x);
+                break;
+            case 4:
+                eliminaDuplicati(V);
+                break;
+            case 5:
+                visualizzaVettore(V);
+                break;
+            case 6:
+                System.out.println("Fine del programma.");
+                break;
+            default:
+                System.out.println("Riprovare");
+                break;
+            }
+        } while(scelta!=6);
+        in.close();
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
